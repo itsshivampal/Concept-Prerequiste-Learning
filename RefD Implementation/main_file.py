7c2a1220a5c3044e74d611b925c6680c7783afce
Shivam <shivampa@iitk.ac.in>
2020-01-18 11:51:29 +0530

    updated theta optimizer function

--- RefD Implementation/main_file.py
+++ RefD Implementation/main_file.py
@@ -3,13 +3,27 @@ from library.data_reading import read_data, read_wiki_data
 from library.algorithm_evaluation import evaluate_prereq_estimation
 from library.prereq_calculation import get_prereq_relations
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Define all required parameters here
-subject = "CS"
-theta = 0.02
-method = "refd"
-w_type = "equal"
+def get_plotting(x_label, y_label):
+    plt.plot(x_label, y_label)
+    plt.xlabel('theta_values')
+    plt.ylabel('average_values')
+    plt.title('Accuracy measures')
+    plt.show()
+
+
+def optimize_theta(subject, method, w_type):
+    theta_values = [float("{0:.2f}".format((-0.5 + 0.01*i))) for i in range(100)]
+    average_values = []
+    for theta in theta_values:
+        estimated_results = main_function(subject, theta, method, w_type)
+        average_values.append(estimated_results["accuracy"])
+        print(theta, estimated_results["accuracy"])
+    get_plotting(average_values, theta_values)
+
 
 
 
@@ -23,17 +37,27 @@ def main_function(subject, theta, method, w_type):
     df_estimated = get_prereq_relations(df_pos, df_neg, df_wiki,
                                                 theta, method, w_type, subject)
     # save calculated prerequisite data
-    save_prereq_relation(df_estimated, method, w_type, subject, theta)
+    # save_prereq_relation(df_estimated, method, w_type, subject, theta)
 
     # estimated result evaluation
     estimated_results = evaluate_prereq_estimation(df_pos, df_neg, df_estimated)
 
     # save evaluation results
-    save_evaluation_results(estimated_results, method, w_type, subject, theta)
+    # save_evaluation_results(estimated_results, method, w_type, subject, theta)
 
     return estimated_results
 
 
-estimated_results = main_function(subject, theta, method, w_type)
+# Define all required parameters here
+subject = "CS"
+# theta = 0.05
+method = "refd"
+w_type = "equal"
 
-print(estimated_results)
+optimize_theta(subject, method, w_type)
+
+
+
+# estimated_results = main_function(subject, theta, method, w_type)
+
+# print(estimated_results)
