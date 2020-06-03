dc2af03cdc3fe38bbf8dfab453ffdaeabe1701ce
Shivam Pal <shivampa@iitk.ac.in>
2020-06-03 11:20:49 +0530

    physics refd updated

--- Physics Exp Setup/refd_method/wiki_tfidf_values.py
+++ Physics Exp Setup/refd_method/wiki_tfidf_values.py
@@ -115,5 +115,5 @@ prereq_file = "../output_files/physics_labeled_pairs.csv"
 wiki_data_file = "../output_files/physics_correct_wikipedia_data.csv"
 df_tfidf_matrix = generate_tfidf_values(prereq_file, wiki_data_file)
 
-output_file = "wiki_tfidf_matrix.csv"
+output_file = "../output_files/wiki_tfidf_matrix.csv"
 df_tfidf_matrix.to_csv(output_file)
