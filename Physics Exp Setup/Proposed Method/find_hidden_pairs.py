9cdaca1b683e17e319fd3e921e96c9f0824f7235
Shivam Pal <shivampa@iitk.ac.in>
2020-06-03 11:18:41 +0530

    physics exp setup added

---
+++ Physics Exp Setup/Proposed Method/find_hidden_pairs.py
@@ -0,0 +1,116 @@
+import pandas as pd
+
+
+def get_prereq_pairs(df):
+	concept_list = list(df["concept"])
+	df.set_index("concept", inplace = True)
+	prereq_pairs = {
+		"topic_a": [],
+		"topic_b": [],
+		"score": []
+	}
+	for concept1 in concept_list:
+		for concept2 in concept_list:
+			tfidf_score = float(df.at[concept1, concept2])
+			if tfidf_score > 0.0:
+				prereq_pairs["topic_a"].append(concept1)
+				prereq_pairs["topic_b"].append(concept2)
+				prereq_pairs["score"].append(tfidf_score)
+	return prereq_pairs, concept_list
+
+
+def sort_all_pairs(new_pairs, all_pairs):
+	df = pd.DataFrame(columns = ["topic_a", "topic_b", "score"])
+	for i in range(len(all_pairs)):
+		df = df.append(all_pairs[i], ignore_index = True)
+	for i in range(len(new_pairs)):
+		df = df.append(new_pairs[i], ignore_index = True)
+	df = df.sort_values(by = ["score"])
+	df.drop_duplicates(subset = ["topic_a", "topic_b"], keep = "last", inplace = True)
+	all_data = {}
+	for i in range(df.shape[0]):
+		all_data[i] = {
+			"topic_a": df[["topic_a"]].iloc[i].values[0],
+			"topic_b": df[["topic_b"]].iloc[i].values[0],
+			"score": df[["score"]].iloc[i].values[0]
+		}
+	return all_data
+
+
+def check_relevant_pairs(all_pairs, pair):
+	topic_b = pair["topic_b"]
+	new_pairs = {}
+	index = 0
+	for i in range(len(all_pairs)):
+		if topic_b == all_pairs[i]["topic_a"]:
+			new_pairs[index] = {
+				"topic_a": pair["topic_a"],
+				"topic_b": all_pairs[i]["topic_b"],
+				"score": min(float(pair["score"]), float(all_pairs[i]["score"]))
+			}
+			index += 1
+	pairs_len = len(all_pairs)
+	all_pairs[pairs_len] = pair
+	all_pairs = sort_all_pairs(new_pairs, all_pairs)
+	return all_pairs
+
+
+
+def get_all_pairs(prereq_pairs):
+	all_pairs = {}
+	all_pairs[0] = {
+		"topic_a": prereq_pairs["topic_a"][0],
+		"topic_b": prereq_pairs["topic_b"][0],
+		"score": prereq_pairs["score"][0]
+	}
+	for i in range(1, len(prereq_pairs["topic_a"])):
+	# for i in range(1, 31):
+		print(i)
+		pair = {
+			"topic_a": prereq_pairs["topic_a"][i],
+			"topic_b": prereq_pairs["topic_b"][i],
+			"score": prereq_pairs["score"][i]
+		}
+		all_pairs = check_relevant_pairs(all_pairs, pair)
+	df = pd.DataFrame(columns = ["topic_a", "topic_b", "score"])
+	for i in range(len(all_pairs)):
+		df = df.append(all_pairs[i], ignore_index = True)
+	return df
+
+
+def get_null_matrix(concept_list):
+	data = {"concept": concept_list}
+	for concept in concept_list:
+		data[concept] = [0.0 for i in range(len(concept_list))]
+	df = pd.DataFrame(data)
+	df.set_index("concept", inplace = True)
+	return df
+
+
+
+def get_prereq_matrix(df, concept_list):
+	df_matrix = get_null_matrix(concept_list)
+	for i in range(df.shape[0]):
+		topic_a = df[["topic_a"]].iloc[i].values[0]
+		topic_b = df[["topic_b"]].iloc[i].values[0]
+		score = df[["score"]].iloc[i].values[0]
+		df_matrix.at[topic_a, topic_b] = float(score)
+	return df_matrix
+
+
+def book_name_correction(concept_list, df):
+	for concept in concept_list:
+		df.at[concept, "Physics"] = 1.0
+	for concept in concept_list:
+		df.at["Physics", concept] = 0.0
+	return df
+
+
+def get_all_prereq_pairs(df_match):
+	# df_match = pd.read_csv(first_prereq_file)
+	match_prereq_pairs, concept_list = get_prereq_pairs(df_match)
+	df_match_all_pairs = get_all_pairs(match_prereq_pairs)
+	df_prereq_matrix = get_prereq_matrix(df_match_all_pairs, concept_list)
+
+	df = book_name_correction(concept_list, df_prereq_matrix)
+	return df
---
+++ Physics Exp Setup/Proposed Method/main_file.py
@@ -0,0 +1,158 @@
+import pandas as pd
+
+from title_concept_matching import match_title_concept
+from resolve_hr_index import sort_hr_sections
+from resolve_mc_index import sort_mc_sections
+from concept_match_index import get_index_from_content
+from sort_book_section import sort_book_section_ambiguity
+from content_processing import get_concept_content
+from concept_tfidf_score import get_tfidf_score
+from concept_ranking import get_concept_ranking
+from rank_filter import apply_rank_filter
+from first_prereq_pairs import get_first_prereq_pairs
+from rank_first_prereq import apply_rank_first_prereq
+from find_hidden_pairs import get_all_prereq_pairs
+
+from data_normalisation import normalise_data
+
+
+from calculate_prereq_val import get_labeled_prereq_val
+from result_evaluation import graph_plotting
+
+
+# Required Files
+book_content_file = "../output_files/physics_normalized_content.csv"
+labeled_pairs_file = "../output_files/physics_labeled_pairs.csv"
+wikipedia_data_file = "../output_files/physics_correct_wikipedia_data.csv"
+concepts_file = "../output_files/physics_concepts_ambiguity.csv"
+wiki_tfidf_matrix = "../output_files/wiki_tfidf_matrix.csv"
+testing_data = "../output_files/testing_data.csv"
+
+chapter_distribution = [[2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+                        [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]]
+
+# Step 1: Matching concepts with Title of book
+# df_match_data = match_title_concept(book_content_file, concepts_file)
+# df_match_data.to_csv("data/title_concept_match.csv")
+
+# Step 2: Resolving Hierarchical ambiguity
+# df_match_data = pd.read_csv("data/title_concept_match.csv", encoding = "utf-8")
+# resolve_hr_data = sort_hr_sections(df_match_data, wikipedia_data_file, book_content_file)
+# resolve_hr_data.to_csv("data/resolve_hr_index.csv")
+
+# Step 3: Resolve Multi Chapter Ambiguity
+# resolve_hr_data = pd.read_csv("data/resolve_hr_index.csv", encoding = "utf-8")
+# resolve_mc_data = sort_mc_sections(resolve_hr_data, chapter_distribution, wikipedia_data_file, book_content_file)
+# resolve_mc_data.to_csv("data/resolve_mc_index.csv")
+
+
+# Step 4: Get index of  content through content
+# resolve_mc_data = pd.read_csv("data/resolve_mc_index.csv", encoding = "utf-8")
+# final_indexing_file = get_index_from_content(book_content_file, resolve_mc_data, chapter_distribution)
+# final_indexing_file.to_csv("data/final_index_file.csv")
+
+
+# Step 5: Get final sections of each concept from each book
+# final_indexing_file = pd.read_csv("data/final_index_file.csv")
+# sorted_concept_sections = sort_book_section_ambiguity(final_indexing_file, wikipedia_data_file, book_content_file)
+# sorted_concept_sections.to_csv("data/sorted_final_concepts.csv")
+
+
+# Step 6: Extracting Content for Concepts
+# sorted_concept_sections = pd.read_csv("data/sorted_final_concepts.csv")
+# concept_content = get_concept_content(sorted_concept_sections, book_content_file)
+# concept_content.to_csv("data/concept_content.csv")
+# print("Step 6 done!!")
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+# Step 7: Finding the universal ranking of concepts in book
+# sorted_concept_sections = pd.read_csv("data/sorted_final_concepts.csv")
+# concept_ranking = get_concept_ranking(sorted_concept_sections, book_content_file)
+# concept_ranking.to_csv("data/concept_rank.csv")
+
+
+# Step 8: Apply Rank filtering on extracted concepts
+# concept_ranking = pd.read_csv("data/concept_rank.csv")
+# concept_content = pd.read_csv("data/concept_content.csv")
+# rank_filtered_concepts = apply_rank_filter(concept_content, concept_ranking)
+# rank_filtered_concepts.to_csv("data/rank_filtered_concepts.csv")
+
+
+# Step 9: Finding TF-IDF score of each concept in each document
+# rank_filtered_concepts = pd.read_csv("data/rank_filtered_concepts.csv")
+# tfidf_score = get_tfidf_score(rank_filtered_concepts)
+# tfidf_score.to_csv("data/content_tfidf_score.csv")
+# print(tfidf_score.shape)
+# print("Step 9 done!!")
+
+#----------------------------------------------------------------------------------
+# Step 10: Find 1st pass of TFIDF Score between concepts
+# tfidf_score = pd.read_csv("data/content_tfidf_score.csv")
+# first_prereq_pairs = get_first_prereq_pairs(tfidf_score)
+# first_prereq_pairs.to_csv("data/first_prereq_pairs.csv")
+
+# Step 11: Apply rank filter to make entries 0
+rank_filtered_concepts = pd.read_csv("data/rank_filtered_concepts.csv")
+first_prereq_pairs = pd.read_csv("data/first_prereq_pairs.csv")
+first_rank_filter = apply_rank_first_prereq(first_prereq_pairs, rank_filtered_concepts)
+first_rank_filter.to_csv("data/first_rank_filter.csv")
+
+# Step 12: Data Normalisation
+first_rank_filter = pd.read_csv("data/first_rank_filter.csv")
+normal_data = normalise_data(first_rank_filter)
+normal_data.to_csv("data/normal_data.csv")
+
+# Step 13: Predict values of labeled pairs
+first_prereq_pairs = pd.read_csv("data/normal_data.csv")
+df_wiki_tfidf = pd.read_csv(wiki_tfidf_matrix)
+df_labeled_pairs = pd.read_csv(labeled_pairs_file)
+labeled_prereq_val = get_labeled_prereq_val(df_labeled_pairs, first_prereq_pairs, df_wiki_tfidf)
+labeled_prereq_val.to_csv("data/predicted_prereq.csv")
+
+#----------------------------------------------------------------------------------
+
+# Step 10: Get in-depth prereq pairs
+# tfidf_score = pd.read_csv("data/content_tfidf_score.csv")
+# all_prereq_pairs = get_all_prereq_pairs(tfidf_score)
+# all_prereq_pairs.to_csv("data/all_prereq_pairs.csv")
+
+
+# Step 11: Result Prediction for labeled pairs
+# df_prereq_pairs = pd.read_csv("data/all_prereq_pairs.csv")
+# df_wiki_tfidf = pd.read_csv(wiki_tfidf_matrix)
+# df_labeled_pairs = pd.read_csv(labeled_pairs_file)
+# labeled_prereq_val = get_labeled_prereq_val(df_labeled_pairs, df_prereq_pairs, df_wiki_tfidf)
+# labeled_prereq_val.to_csv("data/predicted_prereq.csv")
+
+#------------------ Proposed Method Completed -----------------------------#
+
+
+
+#------------------ Testing of Results -----------------------------#
+
+
+
+# Step 12: Graph plotting of predicted results
+df_labeled_prereq = pd.read_csv("data/predicted_prereq.csv")
+graph_plotting(df_labeled_prereq, file_name = "data/curve_all.png")
+
+
+#---------------------------------
+# Comparing results with testing data
+# df_prereq_pairs = pd.read_csv("data/all_prereq_pairs.csv")
+# df_first_prereq = pd.read_csv("data/first_rank_filter.csv")
+
+# df_wiki_tfidf = pd.read_csv(wiki_tfidf_matrix)
+# df_testing_data = pd.read_csv(testing_data)
+
+# df_testing = df_testing_data[["topic_a", "topic_b", "relation"]]
+
+# df_labeled_prereq = get_labeled_prereq_val(df_testing, df_prereq_pairs, df_wiki_tfidf)
+# df_labeled_prereq = get_labeled_prereq_val(df_testing, df_first_prereq, df_wiki_tfidf)
+
+# df_labeled_prereq.to_csv("data/test_data_pred.csv")
+# graph_plotting(df_labeled_prereq, file_name = "data/curve_test.png")
